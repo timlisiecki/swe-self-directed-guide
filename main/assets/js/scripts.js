a6115f35c205a172f7ccf01695862b154b13b6c7
Tim <tim.lisiecki@gmail.com>
2020-06-27 13:26:37 -0400

    updated icons and content

--- main/assets/js/scripts.js
+++ main/assets/js/scripts.js
@@ -294,6 +294,32 @@ Math.easeOutElastic = function (t, b, c, d) {
   };
   window.addEventListener('mousedown', detectClick);
 }());
+// File#: _1_anim-menu-btn
+// Usage: codyhouse.co/license
+(function () {
+  var menuBtns = document.getElementsByClassName("js-anim-menu-btn");
+  if (menuBtns.length > 0) {
+    for (var i = 0; i < menuBtns.length; i++) {
+      (function (i) {
+        initMenuBtn(menuBtns[i]);
+      })(i);
+    }
+
+    function initMenuBtn(btn) {
+      btn.addEventListener("click", function (event) {
+        event.preventDefault();
+        var status = !Util.hasClass(btn, "anim-menu-btn--state-b");
+        // Util.toggleClass(btn, "anim-menu-btn--state-b", status);
+        // emit custom event
+        var event = new CustomEvent("anim-menu-btn-clicked", {
+          detail: status,
+        });
+        btn.dispatchEvent(event);
+      });
+    }
+  }
+})();
+
 // File#: _1_immersive-section-transition
 // Usage: codyhouse.co/license
 (function() {
